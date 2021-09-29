f88a0d881ab5a87f5e2cf027bcf5565f88c22da9
vaibhavvarmadv <vaibhavstar009@gmail.com>
2021-09-29 23:57:51 +0530

    port_issue_v2

--- main.py
+++ main.py
@@ -50,4 +50,4 @@ def get_req_parameters():
 if __name__ == '__main__':
 
     port = int(os.environ.get('PORT',5000))
-    app.run(debug = True, host = '0.0.0.0', port = 3400)
+    app.run(debug = True, host = '0.0.0.0', port = port)
